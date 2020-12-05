f404f2336ef62f8903cf69d192beb6d316de20f5
zmunro <zacharymunro2@gmail.com>
2020-12-05 02:08:07 -0500

    golfed day 2

--- advent5_2.py
+++ advent5_2.py
@@ -1,12 +1,3 @@
-from collections import defaultdict
-
-seat_ids = defaultdict(bool)
-with open("files/advent5_input.txt", "r") as in_f:
-    for line in in_f.readlines():
-        seat_row = int(line.strip()[:7].replace("F","0").replace("B", "1"), 2)
-        seat_col = int(line.strip()[-3:].replace("L","0").replace("R", "1"), 2)
-        seat_ids[seat_row * 8 + seat_col] = True
-
-for id_key in list(seat_ids.keys()):
-    if not seat_ids[id_key + 1] and seat_ids[id_key + 2]:
-        print(id_key + 1)
+# Code Golf
+seat_ids = [int(l.translate({76:48,70:48,66:49,82:49}),2) for l in open("5.txt", "r").readlines()]
+print([set(range(min(seat_ids), max(seat_ids))) - set(seat_ids)][0])
