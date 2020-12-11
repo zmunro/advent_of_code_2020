f7dbf3fbeb894a9793a8d3e0bd8c584118a51e23
zmunro <zacharymunro2@gmail.com>
2020-12-11 11:20:37 -0500

    variable renaming

--- advent11_part2.py
+++ advent11_part2.py
@@ -1,22 +1,23 @@
 grid = [l.strip() for l in open("11.txt", 'r').readlines()]
-width = len(grid[0])
-height = len(grid)
+width, height = len(grid[0]), len(grid)
 grid_string = ''.join(grid)
 
-def search_in_direction(grid, row, col, row_dx, col_dx):
-    r_dx = row_dx
-    c_dx = col_dx
+def search_in_direction(grid, row, col, row_dy, col_dx):
+    dy = row_dy
+    dx = col_dx
     while True:
-        if row + r_dx < 0 or col + c_dx < 0 or row + r_dx >= height or col + c_dx >= width:
+        if row + dy < 0 or col + dx < 0 or row + dy >= height or col + dx >= width:
             return "."
-        if grid[(row + r_dx) * width + col + c_dx] != ".":
-            return grid[(row + r_dx) * width + col + c_dx]
-        r_dx += row_dx
-        c_dx += col_dx
+        if grid[(row + dy) * width + col + dx] != ".":
+            return grid[(row + dy) * width + col + dx]
+        dy += row_dy
+        dx += col_dx
 
 def update_pos(grid, row, col):
+    if grid[row * width + col] == ".":
+        return "."
     directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
-    adjacents = [search_in_direction(grid, row, col, i, j) for i,j in directions]
+    adjacents = [search_in_direction(grid, row, col, dy, dx) for dy, dx in directions]
     if grid[row * width + col] == 'L' and adjacents.count('#') == 0:
         return '#'
     if grid[row * width + col] == '#' and adjacents.count('#') >= 5:
@@ -30,8 +31,8 @@ while True:
         for col in range(width):
             new_grid[row * width + col] = update_pos(grid_string, row, col)
     new_grid_string = ''.join(new_grid)
-    if new_grid_string == grid_string:
+    if new_grid == grid_string:
         break
-    grid_string = new_grid_string
+    grid_string = new_grid
 
-print(new_grid_string.count('#'))
+print(new_grid.count('#'))
